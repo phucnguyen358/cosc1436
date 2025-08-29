77870d08e94643f71e0554a8ae02cfaef09827fd
phucnguyen358 <phuc.nguyen358@my.tccd.edu>
2025-08-28 21:34:41 -0500

    8/28/25 PartsOfLanguage

---
+++ classwork/PartsOfLanguage/PartsOfLanguage/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+//Keywords are in blue - have special meaning to language
+/* This is
+   a multiline
+   comment
+*/
+/* This is
+ *a multiline
+ */
+// This is also
+// a multiline
+//comment
+//block -> {}
+//C++ starts top-down
+//C++ is casesentative firstname is not like firstName
+void main()
+{
+    std::cout << "Hello World";
+}
