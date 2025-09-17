f21c3f52175bfac34ec41ae501cdd8f380da7f9b
phucnguyen358 <phuc.nguyen358@my.tccd.edu>
2025-09-16 21:24:24 -0500

    Talked about setprecision 9/16/25

--- classwork/Chapter3/Chapter3/Chapter3.cpp
+++ classwork/Chapter3/Chapter3/Chapter3.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <iomanip>
 
 void main()
+{
+    // get values for X and Y
+    std::cout << "Enter values for X and Y: ";
+
+    double x, y;
+    std::cin >> x >> y;
+    // std::setw(#) sets the width of a colum, only applies to the next line, not consistant
+    // std::left is consistant
+    // std::fixed is consistant
+    // std::setfill 
+    // if not fixed, then precision is total digits
+    // if fixed is used, then precision is on the right of the decimal
+    // setprecision sets how many digits your floating point values are
+    // Function X Y Result
+    //-----------------------
+    std::cout << std::fixed << std::setprecision(4);
+    std::cout << std::setw(9) << std::left << "Function " << std::setw(7) << " X " << std::setw(7) << " Y " << std::setw(12) << "Result" << std::endl;
+    std::cout << std::setw(35) << std::setfill('-') << "" << std::setfill(' ') << std::endl;
+
+    std::cout << std::setw(9) << std::left << "pow " << std::setw(7) << x << std::setw(7) << y << std::setw(12) << pow(x, y) << std::endl;
+    std::cout << std::setw(9) << std::left << "sqrt " << std::setw(7) << x << std::setw(7) << y << std::setw(12) << sqrt(x) << std::endl;
+   
+    std::cout << std::setw(9) << std::left << "ceil " << std::setw(7) << x << std::setw(7) << y << std::setw(12) << ceil(x) << std::endl;
+    std::cout << std::setw(9) << std::left << "floor " << std::setw(7) << x << std::setw(7) << y << std::setw(12) << floor(x) << std::endl;
+
+    std::cout << std::setw(9) << std::left << "round " << std::setw(7) << x << std::setw(7) << y << std::setw(12) << round(x) << std::endl;
+    std::cout << std::setw(9) << std::left << "trunc " << std::setw(7) << x << std::setw(7) << y << std::setw(12) << trunc(x) << std::endl;
+
+
+}
+void ExpressionDemo()
 {
     // Prompt user for radius of circle
     std::cout << "Enter Radius: ";
@@ -59,5 +92,68 @@ void main()
     std::cout << smallValue << " " << largeValue << std::endl;
 
     // Type Coercion - the compiler automatically changes the type of the operators so that both variables match
+    
+    // Type casting - you explicitly convert an expression's type to another type
+
+    // C-style::= (T) E
+    int left = 10;
+    int right = 3;
+
+    double intDivision = 10 / 3; // int
+    intDivision = 10.0 / 3;      // double / int -> double
+    intDivision - 10 / 3.0;      // int / double -> double
+
+    intDivision = left / right;  // int / int -> int
+
+    intDivision = (double)left / right;  // double / int -> double 
+    intDivision = left / (double)right;  //  int/ double -> double 
+
+    intDivision = (double)(left / right);  // int / int -> (double)int 
+
+    intDivision = static_cast<double>(left) / right;  // double / int -> double 
+    intDivision = left / static_cast<double>(right);  //  int/ double -> double 
+
+    // This is wrong and will not produce 5
+    intDivision = (int)"5";
+    //intDivision = static_cast<int>("5"); // Compiler error
+
+    // Math functions
+    // pow(x, y) => power X to the Y power
+    // sqrt(x)   => square root of x
+    // abs(x)    => absolute value of X
+    //
+    // ceil(x)   => Smallest possible integral value >= X
+    // floor(x)  => Largest possible integral value <= X
+    // round(x) => Rounds a float to an int using midpoint rounding
+    // trunc(x) => Truncates a float to an int
+    // exp(x) => exponential E raises to X power, E = 2.17828
+    // log(x) => logarithm, inverse of exp
+    double result;
+    result = pow(5, 3); // 5 * 5 * 5 = 125
+    result = pow(125.0, -3.0); // cube root of 125
+    
+    result = sqrt(100); // 10
+    result = sqrt(pow(5, 2)); // 5
+
+    result = abs(-10); // 10
+    result = abs(10); // 10
+
+    result = ceil(14.5); // 15
+    result = floor(3.1); //3
+
+    result = round(14.5); // 15
+    result = round(3.1); // 3
+
+    result = trunc(14.5); // 14
+    result = trunc(3.1); // 3
+    result = (int)14.5;
+
+    result = exp(4);
+    result = log(exp(4)); //4
+
+
+
+
+
 
 }
