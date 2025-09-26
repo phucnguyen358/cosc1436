08213d2fe5933660c71d336fa5580a586bce2faa
phucnguyen358 <phuc.nguyen358@my.tccd.edu>
2025-09-25 19:12:39 -0500

    Organized my code so it's up to standards.                 hopefully

--- labs/Lab1/GradeCalculator/GradeCalculator.cpp
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -15,77 +15,52 @@ int main()
     std::cout << std::endl;
     std::cout << "Enter name: ";
     std::string name;
-    std::getline(std::cin, name);
-    // getline reads a line of input from the user and stores it in the variable, in this case its inside "name"
-
-    std::cout << "Enter Lab 1 grade: ";
-    int Lab1 = 0;
-    std::cin >> Lab1;
-
-    std::cout << "Enter Lab 2 grade: ";
-    int Lab2 = 0;
-    std::cin >> Lab2;
-
-    std::cout << "Enter Lab 3 grade: ";
-    int Lab3 = 0;
-    std::cin >> Lab3;
-
-    std::cout << "Enter Lab 4 grade: ";
-    int Lab4 = 0;
-    std::cin >> Lab4;
-
-    std::cout << "Enter Exam 1 grade: ";
-    int Exam1 = 0;
-    std::cin >> Exam1;
-
-    std::cout << "Enter Exam 2 grade: ";
-    int Exam2 = 0;
-    std::cin >> Exam2;
-
-    std::cout << "Enter Exam 3 grade: ";
-    int Exam3 = 0;
-    std::cin >> Exam3;
-
-    std::cout << "Enter participation: ";
-    int participation = 0;
-    std::cin >> participation;
-
-    std::cout << "Enter final exam: ";
-    int finalExam = 0;
-    std::cin >> finalExam;
-
-    std::cout << std::endl;
-    std::cout << name << ", your lab grades are: " << std::endl;
-    std::cout << "Lab 1 = " << Lab1 << std::endl;
-    std::cout << "Lab 2 = " << Lab2 << std::endl;
-    std::cout << "Lab 3 = " << Lab3 << std::endl;
-    std::cout << "Lab 4 = " << Lab4 << std::endl;
-
-    std::cout << std::endl;
-    std::cout << name << ", your exam grades are: " << std::endl;
-    std::cout << "Exam 1 = " << Exam1 << std::endl;
-    std::cout << "Exam 2 = " << Exam2 << std::endl;
-    std::cout << "Exam 3 = " << Exam3 << std::endl;
+    std::getline(std::cin, name); // getline reads a line of input from the user and stores it in the variable, in this case its inside "name"
     
+
+    int lab1 = 0, lab2 = 0, lab3 = 0, lab4 = 0;
+    int exam1 = 0, exam2 = 0, exam3 = 0;
+    int participation = 0, finalExam = 0;
+
+    std::cout << "Enter Lab 1 grade: "; std::cin >> lab1;
+    std::cout << "Enter Lab 2 grade: "; std::cin >> lab2;
+    std::cout << "Enter Lab 3 grade: "; std::cin >> lab3;
+    std::cout << "Enter Lab 4 grade: "; std::cin >> lab4;
+
+    std::cout << "Enter Exam 1 grade: "; std::cin >> exam1;
+    std::cout << "Enter Exam 2 grade: "; std::cin >> exam2;
+    std::cout << "Enter Exam 3 grade: "; std::cin >> exam3;
+
+    std::cout << "Enter participation: "; std::cin >> participation;
+    std::cout << "Enter final exam: "; std::cin >> finalExam;
     std::cout << std::endl;
+
+    std::cout << name << ", your lab grades are: " << std::endl;
+    std::cout << "Lab 1 = " << lab1 << std::endl;
+    std::cout << "Lab 2 = " << lab2 << std::endl;
+    std::cout << "Lab 3 = " << lab3 << std::endl;
+    std::cout << "Lab 4 = " << lab4 << std::endl; std::cout << std::endl;
+
+    std::cout << name << ", your exam grades are: " << std::endl;
+    std::cout << "Exam 1 = " << exam1 << std::endl;
+    std::cout << "Exam 2 = " << exam2 << std::endl;
+    std::cout << "Exam 3 = " << exam3 << std::endl; std::cout << std::endl;
+    
     std::cout << name << ", your other grades are: " << std::endl;
     std::cout << "Participation = " << participation << std::endl;
-    std::cout << "Final Exam = " << finalExam << std::endl;
+    std::cout << "Final Exam = " << finalExam << std::endl; std::cout << std::endl;
 
-    // When interacting with doubles and floats, use non whole numbers such as 1.0 or 8.0
-    double labAverage = (Lab1 + Lab2 + Lab3 + Lab4) / 4.0;
-    std::cout << std::endl;
+    double labAverage = (lab1 + lab2 + lab3 + lab4) / 4.0;  // When interacting with doubles and floats, use non whole numbers such as 1.0 or 8.0
+    double examAverage = (exam1 + exam2 + exam3) / 3.0;
+
     std::cout << name << ", your class grade is: " << std::endl;
-    // setprecision set the number of decimals used for floats and doubles probably
-    std::cout << std::fixed << std::setprecision(2);
+    
+    std::cout << std::fixed << std::setprecision(2); // setprecision set the number of decimals used for floats and doubles probably
     std::cout << "Lab Average (65%) = " << labAverage << "%" << std::endl;
-
-    double examAverage = (Exam1 + Exam2 + Exam3) / 3.0;
     std::cout << "Exam Average (20%) = " << examAverage << "%" << std::endl;
-
     std::cout << "Participation (5%) = " << participation << "%" << std::endl;
-
     std::cout << "Final Exam (10%) = " << finalExam << "%" << std::endl;
+
     double classAverage = (labAverage * 0.65) + (examAverage * 0.20) + (participation * 0.05) + (finalExam * 0.10);
     std::cout << "Class Average = " << classAverage << "%" << std::endl;
 }
